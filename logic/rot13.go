d7590adcfaff7114e5b3d8fd6023205d0eb5a2b9
Davud Safarov <davudseferli@gmail.com>
2021-04-06 16:44:51 +0400

    add test for rot13 algo, then implement it in TDD manner

--- logic/rot13.go
+++ logic/rot13.go
@@ -1,1 +1,38 @@
 package logic
+
+import (
+	"strings"
+)
+
+var rune_a int = int(rune('a'))
+var rune_A int = int(rune('A'))
+var rune_n int = int(rune('n'))
+var rune_N int = int(rune('N'))
+var rune_z int = int(rune('z'))
+var rune_Z int = int(rune('Z'))
+
+func Rot13(input string) string {
+	sb := strings.Builder{}
+
+	for _, ch := range input {
+		ascii := int(ch)
+		if (rune_a <= ascii && ascii < rune_n) || (rune_A <= ascii && ascii < rune_N) {
+			_, err := sb.WriteRune(rune(ascii + 13))
+			panicOnError(err)
+			continue
+		}
+		if (rune_n <= ascii && ascii >= rune_z) || (rune_N <= ascii && ascii > rune_Z) {
+			_, err := sb.WriteRune(rune(ascii - 13))
+			panicOnError(err)
+			continue
+		}
+		sb.WriteRune(ch)
+	}
+	return sb.String()
+}
+
+func panicOnError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
--- logic/rot13_test.go
+++ logic/rot13_test.go
@@ -3,5 +3,12 @@ package logic
 import "testing"
 
 func TestRot13(t *testing.T) {
+	input := "qwertyuiopasdfghjklzxcvbnm"
+	actual := Rot13(input)
+	expected := "djreglhvbcnfqstuwxymkpioaz"
+
+	if actual != expected {
+		t.Error("not equal", actual, expected)
+	}
 
 }
